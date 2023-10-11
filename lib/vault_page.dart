ee846b00a2038facdfcf315a4ef6eb4adbdd5e84
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2023-10-11 22:22:34 +0200

    VaultPage support LoadingState

--- lib/vault_page.dart
+++ lib/vault_page.dart
@@ -1,9 +1,8 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:passvault/vault_cubit.dart';
+import 'package:passvault/vault_data.dart';
 import 'package:passvault/vault_item_page.dart';
 import 'package:passvault/vault_state.dart';
 
@@ -14,9 +13,8 @@ class VaultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final vault = context.read<VaultCubit>();
     return Scaffold(
-      appBar: AppBar(title: Text("Your Vault")),
+      appBar: AppBar(title: const Text("Your Vault")),
       body: BlocConsumer<VaultCubit, VaultState>(listener: (context, state) {
         switch (state) {
           case ErrorState _:
@@ -28,50 +26,63 @@ class VaultPage extends StatelessWidget {
             );
         }
       }, builder: (context, state) {
-        final items = (state as OpenState).data;
-        return ListView.separated(
-          itemBuilder: (context, index) {
-            final item = items[index];
-            return ListTile(
-              title: Text(item.name),
-              subtitle: Text(item.username),
-              trailing: PopupMenuButton<MenuAction>(
-                onSelected: (MenuAction action) => action.call(),
-                itemBuilder: (BuildContext context) =>
-                    <PopupMenuEntry<MenuAction>>[
-                  PopupMenuItem<MenuAction>(
-                    value: () =>
-                        Clipboard.setData(ClipboardData(text: item.username)),
-                    child: Text('Copy username'),
-                  ),
-                  PopupMenuItem<MenuAction>(
-                    value: () =>
-                        Clipboard.setData(ClipboardData(text: item.password)),
-                    child: Text('Copy password'),
-                  ),
-                  PopupMenuItem<MenuAction>(
-                    value: () async {
-                      vault.removeItem(item);
-                      vault.save();
-                    },
-                    child: Text('Remove'),
-                  ),
-                ],
-              ),
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) => VaultItemPage(item: item),
-              )),
-            );
-          },
-          separatorBuilder: (context, index) => Divider(),
-          itemCount: items.length,
-        );
+        return switch (state) {
+          OpenState s => ListView.separated(
+              itemBuilder: (context, index) =>
+                  VaultItemListTile(item: s.data[index]),
+              separatorBuilder: (context, index) => Divider(),
+              itemCount: s.data.length,
+            ),
+          LoadingState _ => const Center(child: CircularProgressIndicator()),
+          VaultState s => Text("Unexpected state $s")
+        };
       }),
       floatingActionButton: FloatingActionButton(
         onPressed: () => Navigator.push(
             context, MaterialPageRoute(builder: (context) => VaultItemPage())),
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class VaultItemListTile extends StatelessWidget {
+  const VaultItemListTile({
+    super.key,
+    required this.item,
+  });
+
+  final VaultItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final vault = context.read<VaultCubit>();
+    return ListTile(
+      title: Text(item.name),
+      subtitle: Text(item.username),
+      trailing: PopupMenuButton<MenuAction>(
+        onSelected: (MenuAction action) => action.call(),
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<MenuAction>>[
+          PopupMenuItem<MenuAction>(
+            value: () => Clipboard.setData(ClipboardData(text: item.username)),
+            child: const Text('Copy username'),
+          ),
+          PopupMenuItem<MenuAction>(
+            value: () => Clipboard.setData(ClipboardData(text: item.password)),
+            child: const Text('Copy password'),
+          ),
+          PopupMenuItem<MenuAction>(
+            value: () async {
+              vault.removeItem(item);
+              vault.save();
+            },
+            child: const Text('Remove'),
+          ),
+        ],
+      ),
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => VaultItemPage(item: item),
+      )),
     );
   }
 }
